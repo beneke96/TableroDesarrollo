4c678bf919eb6a7f068159c9b02e0109b826ebe2
Francisco Javier <fran-15-16@hotmail.com>
2018-02-26 12:07:00 +0000

    Tarea 2 casi terminada (Falta añadir SCRUM al proyecto)

--- src/main/java/es/formacion/cip/fran/demo/App.java
+++ src/main/java/es/formacion/cip/fran/demo/App.java
@@ -1,7 +1,9 @@
 package es.formacion.cip.fran.demo;
 
+import java.util.List;
 import java.util.Properties;
 
+import es.formacion.cip.fran.model.Tarea;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -35,5 +37,39 @@ public class App {
 
     Session session = null;
     Transaction tx = null;
+
+    try {
+        session = sessionFactory.openSession();
+        tx = session.beginTransaction();
+
+        // Creamos la tarea
+        Tarea tarea1 = new Tarea("Corregir Bugs","Se necesita Tetear el codigo y ver donde explota");
+
+        // Guardamos la tarea
+        session.save(tarea1);
+
+        // Comitamos los cambios en la base de datos
+        session.flush();
+        tx.commit();
+
+        // Fetching los datos guardados
+        List<Tarea> listaTareas = session.createQuery("from Tarea").list();
+
+        for(Tarea tarea : listaTareas){
+            System.out.println("ID: " + tarea.getId()+ "| Nombre: " + tarea.getNombre() +
+                    " | Descripcion: " + tarea.getDescripcion());
+        }
+
+    }catch (Exception ex){
+        ex.printStackTrace();
+
+        // Volver a dejar los datos como estaban en caso de fallo
+        tx.rollback();
+    }finally {
+        if (session != null){
+            session.close();
+        }
+    }
+
     }
 }
